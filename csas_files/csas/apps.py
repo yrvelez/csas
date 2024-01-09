3ead679ed6be769d7d54cd6d22050c415cd7ad1a
Yamil R. Velez <yrv2004@columbia.edu>
2024-01-09 17:48:13 -0500

    Fixed duplication issue for "easy CSAS" method

--- csas_files/csas/apps.py
+++ csas_files/csas/apps.py
@@ -1,5 +1,10 @@
 from django.apps import AppConfig
 
+
 class CsasConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'csas'
+
+    def ready(self):
+        from . import default_settings  # Import your module
+        default_settings.load_default_settings()
--- csas_files/csas/models.py
+++ csas_files/csas/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
+
 class DynamicIssueQuestion(models.Model):
     question = models.TextField()
     avg_rating = models.FloatField(default=3.0)
     ratings = models.IntegerField(default=1)
-    var_rating = models.FloatField(default=1.0)
+    var_rating = models.FloatField()
     embedding = models.JSONField(default=None)
 
     def __str__(self):
@@ -12,3 +13,23 @@ class DynamicIssueQuestion(models.Model):
 
     class Meta:
         app_label = 'csas'
+
+
+class UserDatabase(models.Model):
+    user_id = models.TextField()
+    question = models.TextField()
+    rating = models.FloatField(default=3.0)
+
+    def __str__(self):
+        return self.id
+
+    class Meta:
+        app_label = 'csas'
+
+
+class GlobalSetting(models.Model):
+    key = models.CharField(max_length=255, unique=True)
+    value = models.TextField()
+
+    def __str__(self):
+        return f"{self.key}: {self.value}"
